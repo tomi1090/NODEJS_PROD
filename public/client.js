89b262d1a5a032662b3449fa6c4c3e6fe2d8725a
Zamir Yehudit <yehudit.zamir@bankleumi.co.il>
2024-07-22 09:34:29 +0300

    Ffirst Commit

---
+++ public/client.js
@@ -0,0 +1,37 @@
+let ws;
+
+function connectWebSocket(){
+    ws = new WebSocket('ws://localhost:3000');
+
+// event
+ ws.onopen = () => {
+      console.log('onopen');  
+ }
+
+ ws.onmessage = (event) => {
+    const chat = document.getElementById('chat');
+    const message = document.createElement('div');
+    const reader = new FileReader();
+   
+    reader.onload = () => {
+        message.textContent = reader.result;
+        chat.appendChild(message);
+       
+    };
+    if (event.data instanceof Blob) {
+        reader.readAsText(event.data);
+    }
+ };
+ws.onclose = () => {
+}
+}
+function sendMessage(){
+    if (ws.readyState === WebSocket.OPEN){
+        const input = document.getElementById('message'); 
+        ws.send(input.value);
+        input.value = '';
+    }
+    
+}
+
+connectWebSocket();
